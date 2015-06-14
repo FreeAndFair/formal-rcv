da99ff52168e964040a235f8be14a670f5c56d53
Chris Jerdonek <chris.jerdonek@gmail.com>
2015-06-14 03:26:21 -0700

    Normalize Oakland statutes.

--- scripts/norm_statute.py
+++ scripts/norm_statute.py
@@ -2,7 +2,7 @@
 
 This script is useful for cleaning up and standardizing the appearance of
 statute text.  The script expects each paragraph to be separated by a
-single blank line.
+single blank line.  The script was tested with Python 3.4.
 
 Usage:
 
@@ -10,7 +10,8 @@ Usage:
 
 """
 
-# Use Python 3.4.
+# TODO: have the script leave the "header" information alone, perhaps with
+# some type of marker text to indicate the start of the actual statute text.
 
 import textwrap
 import sys
